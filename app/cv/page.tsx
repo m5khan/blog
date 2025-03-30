1b03c06cc80697c1ede3e66abf788d79ab12bc60
m5khan <mshoaibkhan91@gmail.com>
2025-03-30 11:56:58 +0200

    add contact info

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -1,5 +1,12 @@
-import { MapPinIcon, PinIcon } from "lucide-react";
+import {
+  GithubIcon,
+  Linkedin,
+  MailIcon,
+  MapPinIcon,
+  PhoneIcon,
+} from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function CV() {
@@ -7,7 +14,7 @@ export default function CV() {
     <div className="flex flex-col items-center bg-zinc-100 divide-solid divide-y-1">
       <main className="w-[210mm] h-[297mm] bg-white">
         <div className="grid grid-cols-[2fr_1fr] h-full">
-          <div className="bg-rose-100">
+          <article className="bg-rose-100">
             <header className="h-32 bg-white p-3">
               <div className="flex gap-5 items-center">
                 <div>
@@ -43,10 +50,41 @@ export default function CV() {
                 </div>
               </div>
             </header>
-          </div>
-          <div className="bg-rose-200">
-            <header className="h-32 bg-blue-500"></header>
-          </div>
+          </article>
+          <aside className="bg-rose-200">
+            <header className="h-32 bg-blue-500 p-3">
+              <div className="grid grid-cols-[1fr_7fr] grid-rows-4 gap-1">
+                <div className="bg-orange-300">
+                  <MailIcon />
+                </div>
+                <div className="bg-orange-300">
+                  <Link href={"mailto: mshoaibkhan91@gmail.com"}>
+                    mshoaibkhan91@gmail.com
+                  </Link>
+                </div>
+                <div className="bg-orange-300">
+                  <PhoneIcon />
+                </div>
+                <div className="bg-orange-300">+49 176 6720 7947</div>
+                <div className="bg-orange-300">
+                  <GithubIcon />
+                </div>
+                <div className="bg-orange-300">
+                  <Link href={"https://github.com/m5khan"}>
+                    github.com/m5khan
+                  </Link>
+                </div>
+                <div className="bg-orange-300">
+                  <Linkedin />
+                </div>
+                <div className="bg-orange-300">
+                  <Link href={"https://linkedin.com/in/m5khan"}>
+                    linkedin.com/in/m5khan
+                  </Link>
+                </div>
+              </div>
+            </header>
+          </aside>
         </div>
       </main>
       <main className="w-[210mm] h-[297mm] bg-white"></main>
