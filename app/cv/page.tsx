0ee7f20c843172d7a827d6a8a68e44b69a7f48d0
m5khan <mshoaibkhan91@gmail.com>
2025-03-30 13:41:25 +0200

    work experience section

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -11,10 +11,10 @@ import React from "react";
 
 export default function CV() {
   return (
-    <div className="flex flex-col items-center bg-zinc-100 divide-solid divide-y-1">
+    <div className="flex flex-col items-center bg-zinc-500 divide-solid divide-y-1">
       <main className="w-[210mm] h-[297mm] bg-white">
         <div className="grid grid-cols-[2fr_1fr] h-full">
-          <article className="bg-rose-100">
+          <article className="bg-zinc-100">
             <header className="h-32 bg-white p-3">
               <div className="flex gap-5 items-center">
                 <div>
@@ -30,10 +30,10 @@ export default function CV() {
                   <p className="text-2xl font-normal">M SHOAIB KHAN</p>
                   <div
                     id="address-container"
-                    className="flex gap-2 items-center my-1"
+                    className="flex gap-2 items-center my-2"
                   >
                     <div id="place-icon">
-                      <MapPinIcon className="text-gray-500" />
+                      <MapPinIcon strokeWidth={1} size={20} />
                     </div>
                     <div id="address" className="">
                       <p className="text-sm leading-4">
@@ -50,34 +50,83 @@ export default function CV() {
                 </div>
               </div>
             </header>
-          </article>
-          <aside className="bg-rose-200">
-            <header className="h-32 bg-blue-500 p-3">
-              <div className="grid grid-cols-[1fr_7fr] grid-rows-4 gap-1">
-                <div className="bg-orange-300">
-                  <MailIcon />
+            <section id="experience" className="p-3">
+              <h2 className="text-md">WORK EXPERIENCE</h2>
+              {/* <Separator /> */}
+              <div id="experience-card" className="my-2">
+                <div
+                  id="exp-heading"
+                  className="grid grid-cols-[2fr_1fr] items-baseline"
+                >
+                  <div>
+                    <p className="font-medium text-lg leading-5">
+                      Smart In Media AG
+                    </p>
+                    <p>Software Engineer</p>
+                  </div>
+                  <div className="text-end">
+                    <p>Since 07. 2020</p>
+                    <div className="flex items-center gap-0.5 justify-end">
+                      {/* <MapPinIcon size={14} strokeWidth={1.5} /> */}
+                      <p className="text-sm">Cologne, Germany</p>
+                    </div>
+                  </div>
                 </div>
-                <div className="bg-orange-300">
+                <div id="exp-content" className="text-gray-600 text-sm my-2">
+                  <ul className="list-disc ps-4 text-justify">
+                    <li>
+                      Re-planned and implemented database models to achieve
+                      scalability and better adaptation to modification
+                    </li>
+                    <li>
+                      Revamped the User experience to decrease information loss
+                      and increased robustness
+                    </li>
+                    <li>
+                      Introduced typescript in the solution for better
+                      maintainability, scalability, and error prevention
+                    </li>
+                    <li>
+                      Implemented containerization-based deployment in CI/CD
+                      pipeline to increase availability in case of accidents
+                    </li>
+                    <li>
+                      First-hand engagement with the client, understanding the
+                      requirements, proposing solutions, planning the timeline
+                      in Agile-based software development
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </section>
+          </article>
+          <aside className="bg-white">
+            <header className="h-32 p-3 bg-zinc-100">
+              <div className="grid grid-cols-[1fr_7fr] grid-rows-4 gap-0.5 items-center">
+                <div>
+                  <MailIcon size={19} strokeWidth={1} />
+                </div>
+                <div>
                   <Link href={"mailto: mshoaibkhan91@gmail.com"}>
                     mshoaibkhan91@gmail.com
                   </Link>
                 </div>
-                <div className="bg-orange-300">
-                  <PhoneIcon />
+                <div>
+                  <PhoneIcon strokeWidth={1} size={19} />
                 </div>
-                <div className="bg-orange-300">+49 176 6720 7947</div>
-                <div className="bg-orange-300">
-                  <GithubIcon />
+                <div>+49 176 6720 7947</div>
+                <div>
+                  <GithubIcon strokeWidth={1} size={19} />
                 </div>
-                <div className="bg-orange-300">
+                <div>
                   <Link href={"https://github.com/m5khan"}>
                     github.com/m5khan
                   </Link>
                 </div>
-                <div className="bg-orange-300">
-                  <Linkedin />
+                <div>
+                  <Linkedin strokeWidth={1} size={19} />
                 </div>
-                <div className="bg-orange-300">
+                <div>
                   <Link href={"https://linkedin.com/in/m5khan"}>
                     linkedin.com/in/m5khan
                   </Link>
